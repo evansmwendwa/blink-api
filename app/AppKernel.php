ff8c70a090ff1d98e565277eb8a9737e845a1aed
evansmwendwa <evansmwendwa@live.com>
2017-02-12 11:45:34 +0300

    added bundles EasyAdminBundle, VichUploaderBundle, IvoryCKEditorBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
+            new Vich\UploaderBundle\VichUploaderBundle(),
+            new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
             new AppBundle\AppBundle(),
         ];
 
