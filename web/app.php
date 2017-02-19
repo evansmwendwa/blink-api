6349bc450a245f492447fc67a2575d3ed9cbce9d
evansmwendwa <evansmwendwa@live.com>
2017-02-19 23:58:29 +0300

    combined front controller to use dotenv for env specific bootstrapping

--- web/app.php
+++ web/app.php
@@ -1,12 +1,36 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+use Symfony\Component\Dotenv\Dotenv;
 
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../var/bootstrap.php.cache';
 
-$kernel = new AppKernel('prod', false);
+// load environment from envfile
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/../.env');
+$environment = getenv('ENVIRONMENT');
+
+if($environment === 'dev') {
+    if (isset($_SERVER['HTTP_CLIENT_IP'])
+        || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+        || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']) || php_sapi_name() === 'cli-server')
+    ) {
+        header('HTTP/1.0 403 Forbidden');
+        exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+    }
+
+    Debug::enable();
+
+    $kernel = new AppKernel('dev', true);
+
+} else {
+    include_once __DIR__.'/../var/bootstrap.php.cache';
+    $kernel = new AppKernel('prod', false);
+
+}
+
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
